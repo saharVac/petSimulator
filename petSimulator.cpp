1919ab143d44b413cfc8581eb244f9cbef931886
Cowlord3001 <christophera1225@aol.com>
2025-04-02 15:04:49 -0400

    Implemented action menu, formatted / revised comments, and reorganized / renamed functions.

--- petSimulator.cpp
+++ petSimulator.cpp
@@ -1,11 +1,16 @@
+#include <cctype>
 #include <iostream>
+#include <string>
 using namespace std;
 
 const int MAX_PETS = 10;
 
-// 1. "Reading in and storing your data in a meaningful way": can store on a
-// dynmaic array of animal structs
-// - define animal struct
+void addPet();
+void removePet();
+void viewPets();
+
+//* Requirement #1: Read in and store your data in a meaningful way.
+//* Solution: Defining and using a dynamic array of animal structs.
 struct Animal {
   string name;
   int age;
@@ -16,25 +21,57 @@ struct Animal {
   int satiationThresholdPercentage;
   int satiationDecrementTimeMinutes;
 };
-// - iterating over lines selectionOptions.txt, store data in animal struct on
-// the dynamic array
+// - iterate over selectionOptions.txt, store data in the animal struct array.
 
-// 2. "Providing some limited set of interactions for the user that utilizes the
-// data": function to prompt user with action menu
-// - function for prompting menu to user (starting with just add pet, remove
-// pet, and will add more later on)
+//* Requirement #2: Provide interactions for the user that utilizes the data.
+//* Solution: Function to prompt user with action menu (add, remove, view...).
+void actionMenu(Animal* pets) {
+  string choice;
+  cout << "Welcome. Please input one of the following options:" << endl;
+  while (true) {
+    cout << "- Add Pet" << endl
+         << "- Remove Pet" << endl
+         << "- View Pets" << endl
+         << "- Quit" << endl;
+    getline(cin, choice);
+
+    // Converts string to uppercase
+    for (int i = 0; i < choice.size(); i++) choice[i] = toupper(choice[i]);
+
+    if (choice == "A" || choice == "ADD" || choice == "ADD PET")
+      addPet();
+    else if (choice == "R" || choice == "REMOVE" || choice == "REMOVE PET")
+      removePet();
+    else if (choice == "V" || choice == "VIEW" || choice == "VIEW PETS")
+      viewPets();
+    else if (choice == "Q" || choice == "QUIT")
+      break;
+    else
+      cout << "Invalid Response. Please input one of the following options:"
+           << endl;
+  }
+}
+
+void addPet() { cout << "Add Pet" << endl; }
 // - function to handle adding pet: taking in name for pet
+void removePet() { cout << "Remove Pet" << endl; }
 // - function to handle removing pet
 
-// 3. "Providing some basic output to the user in response to their
-// interactions": displaying pets list after every addition/removal of a pet
+//* Requirement #3: Basic output to the user in response to their interactions.
+//* Solution: Function to display pet list.
+void viewPets() { cout << "View Pets" << endl; }
 // - function to display pet list
-void displayPets();
 
 int main() {
-  // - define dynamic array of the animal struct to hold the data
+  // Defines dynamic array of animal struct to hold data.
   Animal* pets = new Animal[MAX_PETS];
+  // for (int i = 0; i < MAX_PETS; i++)
+  // {
+  //   pets[i].
+  // }
 
+  // Opens the action menu.
+  actionMenu(pets);
   // Game loop
 
   return 0;
