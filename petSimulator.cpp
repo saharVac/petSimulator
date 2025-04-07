85306ba2dcfdc5ac5bc66f5954bb2ed0161d048c
Cowlord3001 <christophera1225@aol.com>
2025-04-07 16:32:38 -0400

    Major Changes (see full changelog).

--- petSimulator.cpp
+++ petSimulator.cpp
@@ -3,28 +3,38 @@
 #include <string>
 using namespace std;
 
+// Global vars
+int currentPets = 0;
 const int MAX_PETS = 10;
 
-void addPet();
-void removePet();
-void viewPets();
-
 //* Requirement #1: Read in and store your data in a meaningful way.
-//* Solution: Defining and using a dynamic array of animal structs.
+//  Solution: Defining and using a dynamic array of animal structs.
+//* Requirement #2: Provide interactions for the user that utilizes the data.
+//  Solution: Functions to add / remove pets from the dynamic array.
+//* Requirement #3: Basic output to the user in response to their interactions.
+//  Solution: Function to print the contents of the dynamic array.
+
+//* - TODO: iterate over selectionOptions.txt, store data in the dynamic array.
+
 struct Animal {
   string name;
-  int age;
-  int happinessPercentage;
-  int happinessThresholdPercentage;
-  int happinessDecrementTimeMinutes;
-  int satiationPercentage;
-  int satiationThresholdPercentage;
-  int satiationDecrementTimeMinutes;
+  // int age;
+  // int happinessPercentage;
+  // int happinessThresholdPercentage;
+  // int happinessDecrementTimeMinutes;
+  // int satiationPercentage;
+  // int satiationThresholdPercentage;
+  // int satiationDecrementTimeMinutes;
 };
-// - iterate over selectionOptions.txt, store data in the animal struct array.
 
-//* Requirement #2: Provide interactions for the user that utilizes the data.
-//* Solution: Function to prompt user with action menu (add, remove, view...).
+void actionMenu(Animal*);
+void addPet(Animal*);
+void removePet(Animal*);
+void viewPets(Animal*);
+void quitProgram(Animal*);
+
+// @brief Prompts the user with an action menu (add, remove, view...).
+// @param pets Dynamic array of Animal structs.
 void actionMenu(Animal* pets) {
   string choice;
   cout << "Welcome. Please input one of the following options:" << endl;
@@ -38,39 +48,69 @@ void actionMenu(Animal* pets) {
     // Converts string to uppercase
     for (int i = 0; i < choice.size(); i++) choice[i] = toupper(choice[i]);
 
+    // Action menu
     if (choice == "A" || choice == "ADD" || choice == "ADD PET")
-      addPet();
+      addPet(pets);
     else if (choice == "R" || choice == "REMOVE" || choice == "REMOVE PET")
-      removePet();
+      removePet(pets);
     else if (choice == "V" || choice == "VIEW" || choice == "VIEW PETS")
-      viewPets();
+      viewPets(pets);
     else if (choice == "Q" || choice == "QUIT")
-      break;
+      quitProgram(pets);
     else
       cout << "Invalid Response. Please input one of the following options:"
            << endl;
   }
 }
 
-void addPet() { cout << "Add Pet" << endl; }
-// - function to handle adding pet: taking in name for pet
-void removePet() { cout << "Remove Pet" << endl; }
-// - function to handle removing pet
+// @brief Adds a pet to the dynamic array.
+// @param pets Dynamic array of Animal structs.
+void addPet(Animal* pets) {
+  if (currentPets >= 10)
+    cout << "Pets at capacity! Please remove a pet first." << endl;
+  else {
+    pets[currentPets].name = "Pet #" + to_string(1 + currentPets);
+    // cout << pets[currentPets].name << endl;  //* Debug
+    currentPets++;
+  }
+}
 
-//* Requirement #3: Basic output to the user in response to their interactions.
-//* Solution: Function to display pet list.
-void viewPets() { cout << "View Pets" << endl; }
-// - function to display pet list
+// @brief Removes a pet from the dynamic array.
+// @param pets Dynamic array of Animal structs.
+void removePet(Animal* pets) {
+  if (currentPets <= 0)
+    cout << "No pets remaining! Please add a pet first." << endl;
+  else {
+    currentPets--;
+    pets[currentPets].name = "null";
+  }
+}
+
+// @brief Prints the dynamic array.
+// @param pets Dynamic array of Animal structs.
+void viewPets(Animal* pets) {
+  if (currentPets == 0)
+    cout << "You have no pets!" << endl;
+  else {
+    cout << "You have the following pets:" << endl;
+    for (int i = 0; i < currentPets; i++) cout << pets[i].name << endl;
+  }
+}
+
+// @brief Deallocates the dynamic array and quits the program.
+// @param pets Dynamic array of Animal structs.
+void quitProgram(Animal* pets) {
+  delete[] pets;
+  exit(1);
+}
 
 int main() {
   // Defines dynamic array of animal struct to hold data.
   Animal* pets = new Animal[MAX_PETS];
-  // for (int i = 0; i < MAX_PETS; i++)
-  // {
-  //   pets[i].
-  // }
+  for (int i = 0; i < MAX_PETS; i++) {
+    pets[i].name = "null";
+  }
 
-  // Opens the action menu.
   actionMenu(pets);
   // Game loop
 
